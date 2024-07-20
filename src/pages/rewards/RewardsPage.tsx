0fa1bdb8960ba7eac8b0443210bb70d69256f83e
surbhi-teblox <surbhi.jain@teblox.com>
2024-07-20 19:10:46 +0530

    style: rewards page

--- src/pages/rewards/RewardsPage.tsx
+++ src/pages/rewards/RewardsPage.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
-import CommingSoonComponent from '../../components/ComingsoonComponent';
+import styled from 'styled-components';
+import { DefaultTheme } from '../../styles/Theme';
+import Card from './../../components/Rewards/Card'; // Adjust the import path based on your project structure
+
+const RewardsContainer = styled.div<{ theme: DefaultTheme }>`
+  padding: 40px 20px;
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+const Title = styled.h1<{ theme: DefaultTheme }>`
+  font-size: 38px;
+  margin-bottom: 10px;
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+`;
+
+const Description = styled.p<{ theme: DefaultTheme }>`
+  font-size: 20px;
+  margin-bottom: 40px;
+  color: ${({ theme }) => theme.colors.grey};
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+  flex-wrap: wrap;
+`;
 
 const RewardsPage: React.FC = () => {
   return (
-    <>
-      <CommingSoonComponent />
-    </>
+    <RewardsContainer>
+      <Title>Rewards</Title>
+      <Description>
+        Claim rewards from locking tokens, emissions, incentives, and fees of
+        your voted pools.
+      </Description>
+      <CardContainer>
+        <Card
+          title="Emissions"
+          positionLabel="LP Position"
+          positionValue="0.00"
+          valueLabel="Value Earned"
+          valueValue="0.00"
+        />
+        <Card
+          title="Fees"
+          positionLabel="Tokens Earned"
+          positionValue="0.00"
+          valueLabel="Value Earned"
+          valueValue="0.00"
+        />
+        <Card
+          title="Incentives"
+          positionLabel="Tokens Earned"
+          positionValue="0.00"
+          valueLabel="Value Earned"
+          valueValue="0.00"
+        />
+      </CardContainer>
+    </RewardsContainer>
   );
 };
 
