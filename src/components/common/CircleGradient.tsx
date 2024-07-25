37a6ee91a8a3c4df1f04e165e5c140e82d1c746f
surbhi-teblox <surbhi.jain@teblox.com>
2024-07-25 12:34:09 +0530

    dev: added circle gradient common component in home page

---
+++ src/components/common/CircleGradient.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import circle from '../../assets/circle_gradient.svg';
+
+interface ImageWrapperProps {
+  top: string;
+  left: string;
+}
+
+const ImageWrapper = styled.img<ImageWrapperProps>`
+  z-index: -1;
+  position: absolute;
+  top: ${({ top }) => top};
+  left: ${({ left }) => left};
+`;
+
+interface CircleGradientProps {
+  top: string;
+  left: string;
+}
+
+const CircleGradient: React.FC<CircleGradientProps> = ({ top, left }) => {
+  return <ImageWrapper src={circle} alt="gradient" top={top} left={left} />;
+};
+
+export default CircleGradient;
