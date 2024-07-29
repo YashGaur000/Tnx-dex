02b2c34079fefff49c2af06d1875603e120a590e
BhoodevTeblox <bhoodev.vidua@teblox.com>
2024-07-29 15:24:37 +0530

    Update SwapPage.tsx

--- src/pages/swap/SwapPage.tsx
+++ src/pages/swap/SwapPage.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import styled from 'styled-components';
-import SwapForm from '../../components/Swap/modules/SwapForm';
-import Sidebar from '../../components/Swap/modules/Sidebar';
-import { DefaultTheme } from '../../styles/Theme';
+//import styled from 'styled-components';
+//import SwapForm from '../../components/Swap/modules/SwapForm';
+//import Sidebar from '../../components/Swap/modules/Sidebar';
+//import { DefaultTheme } from '../../styles/Theme';
+//import LiquityRouting from '../../components/Swap/modules/LiquityRouting';
 import { ComingSoon } from '../../components/common';
 
-const SwapContainer = styled.div<{ theme: DefaultTheme }>`
+/*const SwapContainer = styled.div<{ theme: DefaultTheme }>`
   display: flex;
   justify-content: center;
   align-items: flex-start;
   gap: 20px;
   padding: 40px;
   color: ${({ theme }) => theme.colors.text};
-  height: 100vh;
-  @media (max-width: 900px) {
+  height: 135vh;
+  @media (max-width: 1024px) {
     display: inline;
     text-align: -webkit-center;
   }
 `;
+
+const SwapFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;*/
 
 const SwapPage: React.FC = () => {
   return (
@@ -25,15 +32,6 @@ const SwapPage: React.FC = () => {
       <ComingSoon />
     </>
   );
-}; 
-
-/*{
-  return (
-    <SwapContainer>
-      <SwapForm />
-      <Sidebar />
-    </SwapContainer>
-  );
-};*/
+};
 
 export default SwapPage;
