3280232326acbf516879b96d9272db1c140d2fae
mandeep <minturajmdb1999@gmail.com>
2024-07-30 13:23:31 +0530

    update lockScreen

--- src/components/ManageVeTenex/TableContainer.tsx
+++ src/components/ManageVeTenex/TableContainer.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import { DefaultTheme } from '../../styles/Theme';
+import Table from '../common/TableStyled';
 
-interface ReusableTableProps {
+interface TableProps {
   data: Record<string, string | number | string[]>[];
 }
 
@@ -15,83 +16,7 @@ const Section = styled.section<{ theme: DefaultTheme }>`
     background: none;
   }
 `;
-const Table = styled.table<{ theme: DefaultTheme }>`
-  width: 90%;
-  border-collapse: collapse;
-  margin-left: 10px;
 
-  font-family: Kanit;
-  font-size: 15px;
-  font-weight: 300;
-  line-height: 29.9px;
-  color: #ffffff;
-
-  td {
-    padding: 10px 0px;
-    text-align: right;
-  }
-
-  th {
-    background: linear-gradient(209.3deg, #16c062 7.44%, #3eacfc 86.34%);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    background-clip: text;
-    text-align: right;
-
-    padding: 30px 0px;
-  }
-  @media screen and (max-width: 1000px) {
-    font-size: 14px;
-  }
-  @media screen and (max-width: 800px) {
-    &,
-    thead,
-    tbody,
-    th,
-    td,
-    tr {
-      display: block;
-      font-size: 17px;
-      width: 100%;
-    }
-
-    thead tr {
-      position: absolute;
-      top: -9999px;
-      left: -9999px;
-    }
-
-    tr {
-      padding: 20px;
-      margin-top: 20px;
-      border-radius: 10px;
-      background: ${({ theme }) => theme.colors.card};
-    }
-
-    td {
-      border: none;
-
-      position: relative;
-      padding-left: 50%;
-    }
-
-    td:before {
-      position: absolute;
-      top: 50%;
-      left: 10px;
-      transform: translateY(-50%);
-      content: attr(data-label);
-      background: linear-gradient(209.3deg, #16c062 7.44%, #3eacfc 86.34%);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-      background-clip: text;
-    }
-  }
-
-  @media screen and (max-width: 500px) {
-    font-size: 13px;
-  }
-`;
 const Button = styled.button`
   background: linear-gradient(180deg, #273762 0%, #2a3e75 100%);
   height: 24px;
@@ -109,7 +34,7 @@ const Button = styled.button`
   border-radius: 5px;
 `;
 
-const TableContainer: React.FC<ReusableTableProps> = ({ data }) => {
+const TableContainer: React.FC<TableProps> = ({ data }) => {
   if (data.length === 0) return null;
 
   const headers = Object.keys(data[0]);
