722033fb01c29cb30efec38693d30aff91e15ad3
surbhi-teblox <surbhi.jain@teblox.com>
2024-08-01 18:00:35 +0530

    feat: added zustand and state management for header main tabs

---
+++ src/store/slices/headerSlice.ts
@@ -0,0 +1,34 @@
+// src/store/slices/headerSlice.ts
+import create from 'zustand';
+
+interface HeaderState {
+  showTradeSubTabs: boolean;
+  showLiquiditySubTabs: boolean;
+  showGovernanceSubTabs: boolean;
+  showToolsSubTabs: boolean;
+  navOpen: boolean;
+  activeMainTab: string;
+  setActiveMainTab: (tab: string) => void;
+  setShowTradeSubTabs: (value: boolean) => void;
+  setShowLiquiditySubTabs: (value: boolean) => void;
+  setShowGovernanceSubTabs: (value: boolean) => void;
+  setShowToolsSubTabs: (value: boolean) => void;
+  setNavOpen: (value: boolean) => void;
+}
+
+export const useHeaderStore = create<HeaderState>((set) => ({
+  showTradeSubTabs: false,
+  showLiquiditySubTabs: false,
+  showGovernanceSubTabs: false,
+  showToolsSubTabs: false,
+  navOpen: false,
+  activeMainTab: '',
+  setActiveMainTab: (tab: string) => set({ activeMainTab: tab }),
+  setShowTradeSubTabs: (value: boolean) => set({ showTradeSubTabs: value }),
+  setShowLiquiditySubTabs: (value: boolean) =>
+    set({ showLiquiditySubTabs: value }),
+  setShowGovernanceSubTabs: (value: boolean) =>
+    set({ showGovernanceSubTabs: value }),
+  setShowToolsSubTabs: (value: boolean) => set({ showToolsSubTabs: value }),
+  setNavOpen: (value: boolean) => set({ navOpen: value }),
+}));
