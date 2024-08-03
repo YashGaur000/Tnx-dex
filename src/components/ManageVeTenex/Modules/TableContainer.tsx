cf5ff215362e06e68ba413cef8da5a6f957e6697
mandeep <minturajmdb1999@gmail.com>
2024-08-03 16:00:47 +0530

    update lockScreen <add pagination feature>

--- src/components/ManageVeTenex/Modules/TableContainer.tsx
+++ src/components/ManageVeTenex/Modules/TableContainer.tsx
@@ -7,7 +7,7 @@ import SplitScreen from './SplitScreen';
 import PopupScreen from './PopupScreen';
 import MergeLockScreen from './MergeLockScreen';
 import Pagination from './Pagination';
-import { Card } from '../../common';
+import { DefaultTheme } from '../../../styles/Theme';
 
 interface TableProps {
   data: Record<string, string | number | string[]>[];
@@ -28,7 +28,13 @@ const AmountWithImg = styled.div`
 const ButtonContain = styled.div`
   display: inline;
 `;
+const TableContains = styled.div<{ theme: DefaultTheme }>`
+  background: ${({ theme }) => theme.colors.card};
+  margin-top: 20px;
 
+  height: autopx;
+  border-radius: 15px;
+`;
 const TableContainer: React.FC<TableProps> = ({ data }) => {
   const [isPopUpVisible, setPopUpVisible] = useState(false);
   const [isMergeVisible, setIsMergeVisible] = useState(false);
@@ -52,54 +58,53 @@ const TableContainer: React.FC<TableProps> = ({ data }) => {
 
   const headers = Object.keys(data[0]);
   return (
-    <>
-      <Card margin={'20px 0px'} height="1000px">
-        <TableContainerStyle>
-          <Table padding="20px" margin="0px 0px">
-            <thead>
-              <tr>
-                {headers.map((header, index) => (
-                  <th key={index}>{header}</th>
+    <TableContains>
+      <TableContainerStyle>
+        <Table padding="20px" margin="0px 0px">
+          <thead>
+            <tr>
+              {headers.map((header, index) => (
+                <th key={index}>{header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {headers.map((header, colIndex) => (
+                  <td key={`${rowIndex}-${colIndex}`} data-label={header}>
+                    {Array.isArray(row[header]) ? (
+                      row[header].map((item, index) => (
+                        <ButtonContain
+                          key={index}
+                          onClick={() => handleButton(item)}
+                        >
+                          <GradientButton padding="0px 10px" fontSize="13">
+                            {item}
+                          </GradientButton>
+                          <span> &nbsp;&nbsp;</span>
+                        </ButtonContain>
+                      ))
+                    ) : header === 'Locked Amount ' ? (
+                      <AmountWithImg key={header}>
+                        <label>{row[header]}</label> <Img src={TenexIcon} />
+                      </AmountWithImg>
+                    ) : (
+                      row[header]
+                    )}
+                  </td>
                 ))}
               </tr>
-            </thead>
-            <tbody>
-              {data.map((row, rowIndex) => (
-                <tr key={rowIndex}>
-                  {headers.map((header, colIndex) => (
-                    <td key={`${rowIndex}-${colIndex}`} data-label={header}>
-                      {Array.isArray(row[header]) ? (
-                        row[header].map((item, index) => (
-                          <ButtonContain
-                            key={index}
-                            onClick={() => handleButton(item)}
-                          >
-                            <GradientButton padding="0px 10px" fontSize="13">
-                              {item}
-                            </GradientButton>
-                            <span> &nbsp;&nbsp;</span>
-                          </ButtonContain>
-                        ))
-                      ) : header === 'Locked Amount ' ? (
-                        <AmountWithImg key={header}>
-                          <label>{row[header]}</label> <Img src={TenexIcon} />
-                        </AmountWithImg>
-                      ) : (
-                        row[header]
-                      )}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </TableContainerStyle>
-        <Pagination />
-      </Card>
+            ))}
+          </tbody>
+        </Table>
+      </TableContainerStyle>
+      <Pagination />
+
       <PopupScreen isVisible={isPopUpVisible} onClose={closeModal}>
         {isMergeVisible ? <MergeLockScreen /> : <SplitScreen />}
       </PopupScreen>
-    </>
+    </TableContains>
   );
 };
 
