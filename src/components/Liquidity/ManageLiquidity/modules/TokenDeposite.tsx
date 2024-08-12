6430257a5e6307b54ceeb41e600492692ce7b1ee
surbhi-teblox <surbhi.jain@teblox.com>
2024-08-12 12:48:54 +0530

    fix: merge issues fixed

--- src/components/Liquidity/ManageLiquidity/modules/TokenDeposite.tsx
+++ src/components/Liquidity/ManageLiquidity/modules/TokenDeposite.tsx
@@ -24,49 +24,53 @@ import {
   Imgstyle,
 } from '../../LiquidityHomePage/styles/LiquidityTable.style';
 import { PoolDataProps } from '../../LiquidityHomePage/Modules/LiquidityPool';
-import { useLiquidityStore } from '../../../../store/slices/liquiditySlice';
 
 const TokenDeposite = () => {
   const location = useLocation();
   const obj: PoolDataProps =
     (location.state as PoolDataProps) || ({} as PoolDataProps);
 
-    const { poolType, selectedToken1, selectedToken2 } = useLiquidityStore();
+  const { poolType, selectedToken1, selectedToken2 } = useLiquidityStore();
 
-    if (selectedToken1 && selectedToken2) {
+  if (selectedToken1 && selectedToken2) {
+    return (
+      <LiquidityCardSection>
+        <DepositeContentWrapper>
+          <DepositeTokenWithImage>
+            <GroupImgContains>
+              <IMG1Contains Top={5} Left={0}>
+                <Imgstyle
+                  src={selectedToken1.logoURI}
+                  // {obj.icon1}
+                />
+              </IMG1Contains>
+              <IMG2Contains Top={5} Left={26}>
+                <Imgstyle
+                  src={selectedToken2.logoURI}
+                  // {obj.icon2}
+                />
+              </IMG2Contains>
+            </GroupImgContains>
 
-  return (
-    <LiquidityCardSection>
-      <DepositeContentWrapper>
-        <DepositeTokenWithImage>
-          <GroupImgContains>
-            <IMG1Contains Top={5} Left={0}>
-              <Imgstyle src={selectedToken1.logoURI}
-              // {obj.icon1}
-               />
-            </IMG1Contains>
-            <IMG2Contains Top={5} Left={26}>
-              <Imgstyle src={selectedToken2.logoURI}
-              // {obj.icon2}
-               />
-            </IMG2Contains>
-          </GroupImgContains>
-
-          <TokenDescription>
-            <LiquidityHeaderTitle fontSize={20}>
-              {/* {obj.pair} */}
-              {selectedToken1.symbol}-{selectedToken2.symbol}
-            </LiquidityHeaderTitle>
-            <TokenStatus>
-              <StatsCardtitle fontSize={12}>
-                {/* {obj.liquidityType} */}
-                {poolType}
+            <TokenDescription>
+              <LiquidityHeaderTitle fontSize={20}>
+                {/* {obj.pair} */}
+                {selectedToken1.symbol}-{selectedToken2.symbol}
+              </LiquidityHeaderTitle>
+              <TokenStatus>
+                <StatsCardtitle fontSize={12}>
+                  {/* {obj.liquidityType} */}
+                  {poolType}
                 </StatsCardtitle>
-              <label>0.01%{obj.stablePercentage}</label>
-              <LiquidityImgStyle width={'15'} height={'15'} src={InformIcon} />
-            </TokenStatus>
-          </TokenDescription>
-        </DepositeTokenWithImage>
+                <label>0.01%{obj.stablePercentage}</label>
+                <LiquidityImgStyle
+                  width={'15'}
+                  height={'15'}
+                  src={InformIcon}
+                />
+              </TokenStatus>
+            </TokenDescription>
+          </DepositeTokenWithImage>
 
           <TokenContainer>
             <StatsCardtitle fontSize={16}>APR</StatsCardtitle>
@@ -74,33 +78,41 @@ const TokenDeposite = () => {
           </TokenContainer>
         </DepositeContentWrapper>
 
-      <DepositeContentWrapper>
-        <LiquidityStyleContainer>
-          <LiquidityHeaderTitle fontSize={16}>Liquidity</LiquidityHeaderTitle>
-          <LiquidityTitle fontSize={12}>
-            {obj.feesDesc} 0{selectedToken1.symbol}
-            <span></span>
-          </LiquidityTitle>
-          <LiquidityTitle fontSize={12}>
-            {obj.feesSubDesc} 0{selectedToken2.symbol}
-            <span></span>
-          </LiquidityTitle>
-        </LiquidityStyleContainer>
+        <DepositeContentWrapper>
+          <LiquidityStyleContainer>
+            <LiquidityHeaderTitle fontSize={16}>Liquidity</LiquidityHeaderTitle>
+            <LiquidityTitle fontSize={12}>
+              {obj.feesDesc} 0{selectedToken1.symbol}
+              <span></span>
+            </LiquidityTitle>
+            <LiquidityTitle fontSize={12}>
+              {obj.feesSubDesc} 0{selectedToken2.symbol}
+              <span></span>
+            </LiquidityTitle>
+          </LiquidityStyleContainer>
 
-        <DepositeStyle>
-          <LiquidityHeaderTitle fontSize={16}>
-            Your Deposits
-          </LiquidityHeaderTitle>
-          <LiquidityTitle fontSize={12}>{obj.balanceDesc}{selectedToken1.symbol}</LiquidityTitle>
-          <LiquidityTitle fontSize={12}>{obj.volumeSubDesc}{selectedToken2.symbol}</LiquidityTitle>
-        </DepositeStyle>
-      </DepositeContentWrapper>
-    </LiquidityCardSection>
-  );}
-  else{
-    return (<>
-    <h3>Error: Please select tokens</h3>
-    </>);
+          <DepositeStyle>
+            <LiquidityHeaderTitle fontSize={16}>
+              Your Deposits
+            </LiquidityHeaderTitle>
+            <LiquidityTitle fontSize={12}>
+              {obj.balanceDesc}
+              {selectedToken1.symbol}
+            </LiquidityTitle>
+            <LiquidityTitle fontSize={12}>
+              {obj.volumeSubDesc}
+              {selectedToken2.symbol}
+            </LiquidityTitle>
+          </DepositeStyle>
+        </DepositeContentWrapper>
+      </LiquidityCardSection>
+    );
+  } else {
+    return (
+      <>
+        <h3>Error: Please select tokens</h3>
+      </>
+    );
   }
 };
 
