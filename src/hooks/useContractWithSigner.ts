223b0f4d0532863d8a5564a495c468098d01f90a
Bhoodev <bhoodev.vidua@teblox.com>
2024-08-16 10:33:59 +0530

    swap fixes

---
+++ src/hooks/useContractWithSigner.ts
@@ -0,0 +1,20 @@
+import { useMemo } from 'react';
+import { Contract, ContractInterface } from '@ethersproject/contracts';
+import { getContract } from './../utils/contract/getContract';
+import { useAccount } from './useAccount';
+
+export const useContractWithSigner = (
+  contractAddress: string,
+  ABI: ContractInterface,
+): Contract | null => {
+  const {chainId, address} =useAccount();
+  return useMemo(() => {
+    if (!contractAddress || !ABI ) return null;
+    try {
+      return getContract(address, contractAddress, ABI, chainId);
+    } catch (error) {
+      console.error('Failed to create contract instance:', error);
+      return null;
+    }
+  }, [contractAddress, ABI]);
+};
