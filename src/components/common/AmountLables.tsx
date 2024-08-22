6a86d1768d35dac35a169e668dc2d5827123199a
surbhi-teblox <surbhi.jain@teblox.com>
2024-08-22 11:06:44 +0530

    fix: color and font weight

--- src/components/common/AmountLables.tsx
+++ src/components/common/AmountLables.tsx
@@ -1,9 +1,10 @@
 import { DefaultTheme, styled } from 'styled-components';
+import theme from '../../styles/Theme';
 
 export const AmountLabel = styled.label<{ theme: DefaultTheme }>`
   color: silver;
   &:hover {
-    color: black;
-    font-weight: bold;
+    color: ${theme.colors.black};
+    font-weight: ${theme.fontWeights.regular};
   }
 `;
