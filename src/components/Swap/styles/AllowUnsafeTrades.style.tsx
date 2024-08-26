a566680df5bac6ae8cf2adc486cf31c868d61995
unknown <bhoodev.vidua@teblox.com>
2024-08-26 18:57:19 +0530

    some card color fixes and text aligment

--- src/components/Swap/styles/AllowUnsafeTrades.style.tsx
+++ src/components/Swap/styles/AllowUnsafeTrades.style.tsx
@@ -2,14 +2,14 @@ import { styled } from 'styled-components';
 import { DefaultTheme } from '../../../styles/Theme';
 
 export const AllowUnsafeWrapper = styled.div<{
-  display: string;
+  display?: string;
   theme: DefaultTheme;
 }>`
   display: ${({ display }) => display};
   background: ${({ theme }) => theme.colors.card};
-  padding: 10px;
-  width: 100%;
-  border-radius: 20px;
+  border-radius: 12px;
+  padding: 20px;
+  width: 350px;
   margin: 15px 0px;
 `;
 
@@ -20,7 +20,7 @@ export const SidebarTitle = styled.h2<{
   font-size: ${({ fontSize }) => fontSize}px;
   font-weight: ${({ theme }) => theme.fontWeights.regular};
   text-align: justify;
-  margin-bottom: 20px;
+  margin-bottom: 10px;
   margin-left: 11px;
   color: ${({ theme }) => theme.colors.text};
 `;
@@ -31,10 +31,12 @@ export const AllowUnsafeListDes = styled.div<{ theme: DefaultTheme }>`
   font-size: 12px;
   font-weight: ${({ theme }) => theme.fontWeights.regular};
   color: ${({ theme }) => theme.colors.textGreyColor};
-  margin-bottom: 20px;
+  opacity: 0.8;
+  margin-bottom: 15px;
+  width: 266px;
   position: relative;
   text-align: justify;
-  padding-left: 30px;
+  padding-left: 12px;
 `;
 export const AllowUnsafeAlign = styled.div`
   display: flex;
@@ -48,6 +50,8 @@ export const SelectToggleUnsafe = styled.button<{ theme: DefaultTheme }>`
   background: transparent;
   color: ${({ theme }) => theme.colors.greyDark};
   border: none;
+  margin-top: 27px;
+  margin-left: 6px;
   border-radius: 10px;
   cursor: pointer;
 `;
